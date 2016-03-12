91920b61abb8db953867fc5004c1110df9a46cde
Ben Kimock <kimockb@gmail.com>
2016-03-12 16:40:49 -0500

    Fixed jumping stars by adding non-integer offset to star image location, reorganized setup code, storing stars in a set to remove memory leak

--- astrotools/sky_sim.py
+++ astrotools/sky_sim.py
@@ -12,6 +12,13 @@ import matplotlib.pyplot as plt
 
 from scipy.misc import imsave
 
+# Image save path
+save_path = os.path.join(os.getcwd(), 'test.png')
+
+# Star psf dimensions
+size = 11
+psf = 2
+
 # Use tkinter to get screen dimensions
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
@@ -21,12 +28,6 @@ screen_radius = np.sqrt(screen_height**2 + screen_width**2)/2
 
 # Initialize image
 image = np.zeros((screen_height+20, screen_width+20))
-
-# Initialize star template
-size = 11
-psf = 2
-dst = np.sqrt((np.arange(size)[:,None]-size/2+.5)**2 + (np.arange(size)-size/2+.5)**2)
-template = np.exp(-dst**2/psf**2)
 
 # Load star locations
 star_data = np.loadtxt('star_list.txt')
@@ -39,6 +40,10 @@ v_mag = star_data[:,3]
 altitude = np.empty_like(right_ascension)
 azimuth = np.empty_like(declination)
 
+stars = set()
+for i in range(len(declination)):
+    stars.add(ephem.readdb('star,f|S,'+str(ephem.hours(right_ascension[i]))+','+str(ephem.degrees(declination[i]))+','+str(v_mag[i])))
+
 while True:
     start = datetime.now()
     # Set up observer so get correct time
@@ -47,11 +52,11 @@ while True:
     location.lon = '-82.325'
 
     # Compute alt and az for each star
-    for i in range(declination.size):
-        star = ephem.readdb('star,f|S,'+str(ephem.hours(right_ascension[i]))+','+str(ephem.degrees(declination[i]))+',0')
+    for s,star in enumerate(stars):
         star.compute(location)
-        altitude[i] = star.alt
-        azimuth[i] = star.az
+        altitude[s] = star.alt
+        azimuth[s] = star.az
+        v_mag[s] = star.mag
 
     # Convert to rectangular screen coordinates
     screen_x = screen_radius * np.cos(azimuth) * np.sin(np.pi/2-altitude)
@@ -59,27 +64,31 @@ while True:
 
     # Filter stars that fit on the screen
     mask = (screen_x > -screen_width/2) & (screen_x < screen_width/2) & (screen_y < screen_height/2) & (screen_y > -screen_height/2) & (altitude > 0)
-    screen_x = (screen_x[mask] + screen_width/2).astype(int)
-    screen_y = (screen_y[mask] + screen_height/2).astype(int)
+    screen_x = screen_x[mask] + screen_width/2
+    screen_y = screen_y[mask] + screen_height/2
 
     part_v_mag = v_mag[mask]
     part_v_mag -= part_v_mag.max()
     part_v_mag *= -1
     part_v_mag = part_v_mag.clip(0,3)
 
-    # Draw the image
     image *= 0
+    screen_y += 10
+    screen_x += 10
     for i in range(screen_y.size):
-        y,x = screen_y[i]+10, screen_x[i]+10
-        image[y-5:y+6, x-5:x+6] += part_v_mag[i]*template
+        y,x = int(screen_y[i]), int(screen_x[i])
+        y_c,x_c = screen_y[i]-y, screen_x[i]-x
+        dst = np.sqrt((np.arange(size)[:,None]-5-y_c)**2 + (np.arange(size)-5-x_c)**2)
+        star_image = np.exp(-dst**2/psf**2)
+        star_image *= part_v_mag[i]/np.sum(star_image)
+        image[y-5:y+6, x-5:x+6] += star_image
 
     image = image.clip(0,part_v_mag.max())
+
     imsave('test.png', image[10:-10,10:-10])
 
     SPI_SETDESKWALLPAPER = 20
-    path = os.path.join(os.getcwd(), 'test.png')
-    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path, 2)
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, save_path, 2)
     print(datetime.now()-start)
 
     time.sleep(1)
-
